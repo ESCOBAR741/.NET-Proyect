638489699522e126251b0f814ce7aea638e9b700
DarkDerrman <cchepillo70@gmail.com>
2023-05-27 22:36:34 -0400

    Proyecto Asp.Net Core
    Proyecto Asp.Net Core los mantenedores del Proyecto Seguimiento de Gastos.

---
+++ ProyAspNetCoreMVC/Models/Departamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyAspNetCoreMVC.Models;
+
+public partial class Departamento
+{
+    public byte IdDepto { get; set; }
+
+    public string NombreDepto { get; set; } = null!;
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
---
+++ ProyAspNetCoreMVC/Models/Gasto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyAspNetCoreMVC.Models;
+
+public partial class Gasto
+{
+    public int IdGasto { get; set; }
+
+    public string TipoGasto { get; set; } = null!;
+
+    public string Detalle { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+
+    public int Monto { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public DateTime? FechaGasto { get; set; }
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+}
