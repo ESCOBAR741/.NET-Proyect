fdbe479924be4087d038ec6fce1447ed97610355
Denilson Escobar <denilsonescobar741@gmail.com>
2023-06-19 23:55:46 -0400

    model loggin add

--- ProyASPNetCoreAdmin/Controllers/CuentaController.cs
+++ ProyASPNetCoreAdmin/Controllers/CuentaController.cs
@@ -1,23 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProyASPNetCoreAdmin.Models;
+using System.Security.Claims;
 
 namespace ProyASPNetCoreAdmin.Controllers
 {
     public class CuentaController : Controller
     {
-       /* private readonly DbseguimientoGastosContext _context;
+        private readonly DbseguimientoGastosContext _context;
+        string mensaje;
 
-        public CuentaController(DbseguimientoGastosContext context)
-        {
-            _context = context;
-        }
-       */
+         public CuentaController(DbseguimientoGastosContext context)
+         {
+             _context = context;
+         }
+        
 
         // GET: CuentaController1
         public ActionResult Index()
         {
             return View("Login");
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = _context.Usuarios.FirstOrDefault(u=>u.Usuario1 == model.Usuario1 && u.Contraseña == model.Contraseña);
+                if(user != null)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim (ClaimTypes.Name,user.Usuario1)
+                    };
+
+                    var identity = new ClaimsIdentity(claims, "Login");
+                    var principal = new ClaimsPrincipal(identity);
+                    HttpContext.SignInAsync(principal).Wait();
+
+                    return RedirectToAction("Index", "Home"); ;
+                }
+                else
+                {
+                    mensaje = "Error de usuuario y/o contraseña";
+                    ViewData["Mensaje"] = mensaje;
+                }
+
+            }
+            return View(model);
         }
 
         // GET: CuentaController1/Details/5
