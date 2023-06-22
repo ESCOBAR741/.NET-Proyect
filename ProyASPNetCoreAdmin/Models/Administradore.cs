f24e6ba69a00512b3228a6f7da1748c3ce1e7f52
Denilson Escobar <denilsonescobar741@gmail.com>
2023-06-22 18:39:51 -0400

    Update Admin Login

--- ProyASPNetCoreAdmin/Models/Administradore.cs
+++ ProyASPNetCoreAdmin/Models/Administradore.cs
@@ -7,15 +7,15 @@ public partial class Administradore
 {
     public byte IdAdmin { get; set; }
 
-    public string NombreAdmin { get; set; } = null!;
+    public string? NombreAdmin { get; set; } = null!;
 
-    public string ContraseñaAdmin { get; set; } = null!;
+    public string? ContraseñaAdmin { get; set; } = null!;
 
-    public string Nombres { get; set; } = null!;
+    public string? Nombres { get; set; } = null!;
 
-    public string Apellidos { get; set; } = null!;
+    public string? Apellidos { get; set; } = null!;
 
-    public string Run { get; set; } = null!;
+    public string? Run { get; set; } = null!;
 
     public virtual ICollection<Departamento> Departamentos { get; set; } = new List<Departamento>();
 
